d0d99da3ff905881d42d0df739d933bfe7638404
Rahatfa <rhtttt123@gmail.com>
2022-11-30 01:55:56 +0600

    loading catagories

--- index.js
+++ index.js
@@ -12,13 +12,27 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6hwmbl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
 
+    try{
+        const catagoriesCollection = client.db('MobileWorld').collection('catagories');
+
+        app.get('/catagories',async(req, res) =>{
+            const query = {};
+            const options = await catagoriesCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log);
 
 
 app.get('/', async(req, res) =>{
